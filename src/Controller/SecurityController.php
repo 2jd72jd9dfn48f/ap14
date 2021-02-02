6e0b275d6f878f1735ab49b02754416435f7a622
hyngwar <seokkjj83@gmail.com>
2021-02-02 15:18:06 +0100

    no message

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,7 +3,12 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\UserRepository;
+use DateTimeInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SecurityController extends AbstractController
@@ -19,5 +24,47 @@ class SecurityController extends AbstractController
             'username' => $user->getUsername(),
             'roles' => $user->getRoles(),
         ]);
+    }
+
+
+}
+
+/**
+ * Class UserController
+ * @package App\Controller
+ *
+ * @Route(path="/api/")
+ */
+class UserController
+{
+    private $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    /**
+     * @Route("users", name="get_all_users", methods={"GET"})
+     */
+    public function getAll(): JsonResponse
+    {
+        $users = $this->userRepository->findAll();
+        $data = [];
+
+        foreach ($users as $user) {
+            $data[] = [
+                'id' => $user->getId(),
+                'name' => $user->getTitulo(),
+                'image' => $user->getImagen(),
+                'description' => $user->getDescription(),
+                'date' => $user->getDate(),
+                'days' => $user->getDays(),
+                'price' => $user->getPrice(),
+                'categoria' => $user->getCategoria(),
+            ];
+        }
+
+        return new JsonResponse($data, Response::HTTP_OK);
     }
 }
